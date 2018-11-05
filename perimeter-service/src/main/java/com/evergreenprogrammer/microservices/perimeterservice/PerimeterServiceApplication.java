ed869ada11410a73bc896f6bb28022632ae46ea1
Tapojit Bhattacharya <tapojit.bhattacharya@gmail.com>
2018-11-05 10:36:52 +0530

    Enabled Eureka client capabilities to the business services.

--- perimeter-service/src/main/java/com/evergreenprogrammer/microservices/perimeterservice/PerimeterServiceApplication.java
+++ perimeter-service/src/main/java/com/evergreenprogrammer/microservices/perimeterservice/PerimeterServiceApplication.java
@@ -2,7 +2,9 @@ package com.evergreenprogrammer.microservices.perimeterservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient(autoRegister = true)
 @SpringBootApplication
 public class PerimeterServiceApplication {
 
--- shape-service/src/main/java/com/evergreenprogrammer/microservices/shapeservice/ShapeServiceApplication.java
+++ shape-service/src/main/java/com/evergreenprogrammer/microservices/shapeservice/ShapeServiceApplication.java
@@ -2,10 +2,12 @@ package com.evergreenprogrammer.microservices.shapeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@EnableDiscoveryClient(autoRegister = true)
 @EnableFeignClients(basePackages = "com.evergreenprogrammer.microservices.shapeservice.proxy")
+@SpringBootApplication
 public class ShapeServiceApplication {
 
 	public static void main(String[] args) {
